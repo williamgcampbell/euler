a3dea862d2f6ae614c9b77b1b2936f42e24df3f1
Will Campbell <wcampbell@homeaway.com>
2013-04-28 01:38:54 -0500

    removing moved files

--- prob005.py
+++
@@ -1,7 +0,0 @@
-n = r = 0
-while not r:
-    n += 20
-    #is the length of the list of elements divisible by n (excluding 1) equal to high - low
-    r = (lambda x: len([i for i in xrange(2, 21) if x % i == 0]) == 19)(n)
-
-print "The smallest positive number evenly divisible by all numbers from 1 to 20 is:", n
--- prob006.py
+++
@@ -1,6 +0,0 @@
-def sumSquareDifference(n, m):
-    sumOfSquares = sum((lambda x, y: [i**2 for i in xrange(x, y+1)])(n, m))
-    squareOfSum = (lambda x, y: sum(xrange(x, y+1))**2)(n, m)
-    return squareOfSum - sumOfSquares
-    
-print "The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is: ", sumSquareDifference(1, 100)
